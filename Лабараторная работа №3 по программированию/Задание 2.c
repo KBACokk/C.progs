6de8b439744fe602c56cfb70d86fe2f0dcedee80
KBACokk <statsenko228s@gmail.com>
2024-10-30 22:10:13 +0700

    Add files via upload

---
+++ Лабараторная работа №3 по программированию/Задание 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d", &a);
+    int s = 0;
+    for (int n = 2; n < a; n++) {
+        if (a % n == 0) { 
+            s = 1;
+            printf("Число не простое"); 
+            break;  
+        }
+    }
+}
